83a1db38939e948531894b872c20cb104dd49a6b
Elen-M <meribele15@gmail.com>
2024-12-01 06:26:45 +0200

    fixed -hw-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
-boxSizing = (parseFloat(padding) + parseFloat(border)) * 2 + parseFloat(content);
-console.log(boxSizing);
+let boxSizing = (parseFloat(padding) + parseFloat(border)) * 2 + parseFloat(content);
+return boxSizing;
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
